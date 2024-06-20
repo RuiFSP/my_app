bac438f6add05aeb30f11f9777d14dedffbe6223
ruipinto <ruifspinto@gmail.com>
2024-06-20 21:22:44 +0100

    adding port to app

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def recidivism_result():
         return jsonify({"error": "Invalid input data!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
